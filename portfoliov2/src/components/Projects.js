e43e6067d9a42452f06bc4a28758b9f36d23a090
DaltonMcauliffe <dalton.j.mcauliffe97@gmail.com>
2023-12-20 16:45:21 -0800

    made some more changes Prayingit works

--- portfoliov2/src/components/Projects.js
+++ portfoliov2/src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, CardBody, CardSubtitle, CardText, CardTitle, Button} from 'reactstrap'
-import DogLogo from './components/Images/The Woof (1).png'
+import DogLogo from '../components/Images/The Woof (1).png'
 
 const Projects = () => {
  
