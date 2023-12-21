d3b92cd5b82f06deee6d322236a1008c3d5327c2
DaltonMcauliffe <dalton.j.mcauliffe97@gmail.com>
2023-12-20 16:34:53 -0800

    changes

--- portfoliov2/src/components/Projects.js
+++ portfoliov2/src/components/Projects.js
@@ -1,45 +1,48 @@
-import React from "react";
-import {
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardText,
-  CardTitle,
-  Button,
-} from "reactstrap";
-import DogLogo from "/Users/learnacademy/Desktop/portfolio.v2/portfoliov2/src/components/Images/The Woof (1).png";
+import React from 'react'
+import { Card, CardBody, CardSubtitle, CardText, CardTitle, Button} from 'reactstrap'
+import DogLogo from './components/Images/The Woof (1).png'
 const Projects = () => {
+ 
   const handleClick = () => {
-    window.open("https://good-dogs-tinder.onrender.com/", "_blank");
-  };
+    window.open('https://good-dogs-tinder.onrender.com/', '_blank')
+  }
 
   return (
-    <>
-      <div className="Projects-Header">
-        <h1>Projects</h1>
-      </div>
-      <div className="projects-folder">
-        <Card
-          style={{
-            width: "18rem",
-          }}
-        >
-          <img alt="Sample" src={DogLogo} />
-          <CardBody>
-            <CardTitle tag="h5">Dog Tinder</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Created by Dalton and M
-            </CardSubtitle>
-            <CardText>
-              Welcome to Dog Tinder created by Dalton and M come match a puppy
-              on our live website!
-            </CardText>
-            <Button onClick={handleClick}>Click Me!</Button>
-          </CardBody>
-        </Card>
-      </div>
-    </>
-  );
-};
+   <>
+   <div className='Projects-Header'>
+  <h1>Projects</h1>
+  </div>
+   <div className='projects-folder'>
+   <Card
+  style={{
+    width: '18rem',
+  }}
+>
+  <img
+    alt="Sample"
+    src={DogLogo}
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      Dog Tinder
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Created by Dalton and M
+    </CardSubtitle>
+    <CardText>
+      Welcome to Dog Tinder created by Dalton and M come match a puppy on our live website!
+    </CardText>
+    <Button onClick={handleClick}>
+      Click Me!
+    </Button>
+  </CardBody>
+</Card>
+   </div>
+   </>
+  )
+}
 
-export default Projects;
+export default Projects
